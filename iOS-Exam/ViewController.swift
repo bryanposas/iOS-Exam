14b492e7c5a02fce4069cba3279b0ed706c37c6a
bryan posas <bryanposas@gmail.com>
2023-08-07 04:33:33 +0800

    Initial Commit

---
+++ iOS-Exam/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS-Exam
+//
+//  Created by Bryan Posas on 8/7/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
